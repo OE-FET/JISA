d733d0be290d7ddb5bba3c28276546a00db9ffe3
William Wood <waw31@cam.ac.uk>
2018-04-06 15:19:51 +0100

    Added class for Keithley 236 SMU

--- src/JPIB/GPIB.java
+++ src/JPIB/GPIB.java
@@ -6,14 +6,24 @@ import java.util.HashMap;
 
 public class GPIB {
 
-    private static GPIB instance;
-    private static HashMap<Integer, ArrayList<Integer>> buses = new HashMap<>();
-    private static HashMap<Integer, Boolean> initialised = new HashMap<>();
+    // JNI is designed around calls from an object instance (ie not static apparently)
+    private static GPIB                                 instance;
+    private static HashMap<Integer, ArrayList<Integer>> buses       = new HashMap<>();
+    private static HashMap<Integer, Boolean>            initialised = new HashMap<>();
 
+    /**
+     * Load the native library
+     */
     static {
         System.loadLibrary("JPIB");
     }
 
+    /**
+     * Callback for the native scan function
+     *
+     * @param bus busID
+     * @param id  GPIB address of found device
+     */
     public void addFound(int bus, int id) {
 
         if (!buses.containsKey(bus)) {
@@ -24,26 +34,91 @@ public class GPIB {
 
     }
 
+    /**
+     * JNI Call to initialise the bus
+     *
+     * @param bus Bus to initialise
+     *
+     * @throws IOException Upon communication error
+     */
     private native void init(int bus) throws IOException;
 
+    /**
+     * JNI Call to scan for devices
+     *
+     * @param bus Which bus to scan
+     *
+     * @throws IOException Upon communication error
+     */
     private native void scan(int bus) throws IOException;
 
+    /**
+     * JNI Call to open a device
+     *
+     * @param bus     Bus the device is on
+     * @param address Address if the device on the bus
+     * @param timeout What timeout should be used
+     * @param EOI     Does this device send the EOI line at the end of talking?
+     * @param EOS     What does this devices send to signal the end of a string?
+     *
+     * @return The device descriptor ID of the opened device
+     *
+     * @throws IOException Upon communication error
+     */
     private native int open(int bus, int address, int timeout, int EOI, int EOS) throws IOException;
 
+    /**
+     * JNI call to write to the device.
+     *
+     * @param device  The device descriptor ID of the device
+     * @param command The string to write
+     *
+     * @throws IOException Upon communication error
+     */
     private native void write(int device, String command) throws IOException;
 
+    /**
+     * JNI call to address the device to talk and read its response.
+     *
+     * @param device The device descriptor ID of the device
+     *
+     * @return The response of the device
+     *
+     * @throws IOException Upon communication error
+     */
     private native String read(int device) throws IOException;
 
+    /**
+     * Initialise a bus
+     *
+     * @param bus The bus to initialise
+     *
+     * @throws IOException
+     */
     public static void initialise(int bus) throws IOException {
         instance = new GPIB();
         instance.init(bus);
         initialised.put(bus, true);
     }
 
+    /**
+     * Has the given bus been initialised?
+     *
+     * @param bus The bus to check
+     *
+     * @return boolean
+     */
     public static boolean isInitialised(int bus) {
         return initialised.getOrDefault(bus, false);
     }
 
+    /**
+     * Scan for devices on the given bus
+     *
+     * @param bus The bus to scan
+     *
+     * @throws IOException Upon communication error
+     */
     public static void scanForDevices(int bus) throws IOException {
 
         if (buses.containsKey(bus)) {
@@ -56,18 +131,58 @@ public class GPIB {
 
     }
 
+    /**
+     * Open the device on the given bus at the given address and return its unique descriptor ID.
+     *
+     * @param bus     The bus the device is on
+     * @param address The address on the bus the device is using
+     * @param timeout The timeout value for communications with the device
+     * @param EOI     Does this device end communications with an EOI line?
+     * @param EOS     The byte this device uses to signal the end of a string
+     *
+     * @return Device descriptor ID
+     *
+     * @throws IOException Upon communication error
+     */
     public static int openDevice(int bus, int address, int timeout, int EOI, int EOS) throws IOException {
         return instance.open(bus, address, timeout, EOI, EOS);
     }
 
+    /**
+     * Write to the given device.
+     *
+     * @param device  The device descriptor ID of the device
+     * @param command The string to write
+     *
+     * @throws IOException Upon communication error
+     */
     public static void writeCommand(int device, String command) throws IOException {
         instance.write(device, command);
     }
 
+    /**
+     * Address the device to talk and read its response.
+     *
+     * @param device The device descriptor ID of the device to talk
+     *
+     * @return The response string
+     *
+     * @throws IOException Upon communication error
+     */
     public static String readDevice(int device) throws IOException {
         return instance.read(device);
     }
 
+    /**
+     * Write to the device then address it to talk and read its response
+     *
+     * @param device  The device descriptor ID of the device to query
+     * @param command The string to send
+     *
+     * @return The response of the device
+     *
+     * @throws IOException Upon communication error
+     */
     public static String queryDevice(int device, String command) throws IOException {
         instance.write(device, command);
         return instance.read(device);
@@ -93,10 +208,10 @@ public class GPIB {
             return new GPIBDevice[0];
         }
 
-        int count = buses.get(bus).size();
+        int          count   = buses.get(bus).size();
         GPIBDevice[] devices = new GPIBDevice[count];
 
-        for (int i = 0; i < count; i ++) {
+        for (int i = 0; i < count; i++) {
             int address = buses.get(bus).get(i);
             devices[i] = new GPIBDevice(bus, address);
         }
