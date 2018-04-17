0036e5560a3344381cd35e924db7917e5ff9780f
William Wood <waw31@cam.ac.uk>
2018-04-17 09:39:41 +0100

    Moving about the exception handler.

--- src/JPIB/Util.java
+++ src/JPIB/Util.java
@@ -1,5 +1,8 @@
 package JPIB;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Util {
 
     public static boolean isBetween(double value, double min, double max) {
@@ -8,6 +11,61 @@ public class Util {
 
     public static boolean isBetween(int value, int min, int max) {
         return value >= min && value <= max;
+    }
+
+    public static void exceptionHandler(Exception e) {
+
+        ExType exceptionType = ExType.fromClass(e.getClass());
+
+        switch (exceptionType) {
+
+            case IO_EXCEPTION:
+                System.err.printf("Communication error: \"%s\"\n", e.getMessage());
+                break;
+            case DEVICE_EXCEPTION:
+                System.err.printf("Device error: \"%s\"\n", e.getMessage());
+                break;
+            case INTERRUPTED_EXCEPTION:
+                System.err.printf("Waiting error: \"%s\"\n", e.getMessage());
+                break;
+            default:
+                System.err.printf("Unknown error: \"%s\"\n", e.getMessage());
+                break;
+        }
+
+        System.exit(1);
+
+    }
+
+    enum ExType {
+
+        IO_EXCEPTION(IOException.class),
+        DEVICE_EXCEPTION(DeviceException.class),
+        INTERRUPTED_EXCEPTION(InterruptedException.class),
+        UNKNOWN_EXCEPTION(Exception.class);
+
+        private static HashMap<Class, ExType> lookup = new HashMap<>();
+
+        static {
+            for (ExType e : ExType.values()) {
+                lookup.put(e.getClazz(), e);
+            }
+        }
+
+        static ExType fromClass(Class c) {
+            return lookup.getOrDefault(c, UNKNOWN_EXCEPTION);
+        }
+
+        private Class clazz;
+
+        ExType(Class c) {
+            clazz = c;
+        }
+
+        Class getClazz() {
+            return clazz;
+        }
+
     }
 
 }
