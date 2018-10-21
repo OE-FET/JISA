553b2643ecee71255ad785ecd9d06f0e75b3e521
William Wood <waw31@cam.ac.uk>
2018-10-22 00:21:42 +0100

    Input Fields Update.

---
+++ src/JISA/GUI/IntegerField.java
@@ -0,0 +1,31 @@
+package JISA.GUI;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.util.converter.IntegerStringConverter;
+
+import java.util.function.UnaryOperator;
+
+public class IntegerField extends TextField
+{
+
+    private static final UnaryOperator<TextFormatter.Change> INT_FILTER = change -> {
+        String newText = change.getControlNewText();
+        if (newText.matches("[\\-\\+]?([0-9]*)?")) {
+            return change;
+        }
+        return null;
+    };
+
+    public IntegerField() {
+        super();
+        setTextFormatter(new TextFormatter<Integer>(new IntegerStringConverter(), 0, INT_FILTER));
+    }
+
+
+    public int getIntValue() {
+        return Integer.valueOf(getText());
+    }
+
+}
+
