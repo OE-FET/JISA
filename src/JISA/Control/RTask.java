93af2b532bc754b75a651aa3dbad5954a0bc77f8
William Wood <waw31@cam.ac.uk>
2018-12-05 18:10:05 +0000

    RTask, GUI element sizing.

---
+++ src/JISA/Control/RTask.java
@@ -0,0 +1,66 @@
+package JISA.Control;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class RTask {
+
+    private long      interval;
+    private SRunnable toRun;
+    private boolean   running = false;
+    private long      started;
+    private Timer     timer   = new Timer();
+    private TimerTask task;
+
+    public RTask(long interval, SRunnable toRun) {
+        this.interval = interval;
+        this.toRun = toRun;
+    }
+
+    public synchronized void start() {
+
+        if (running) {
+            return;
+        }
+        started = System.currentTimeMillis();
+
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    toRun.run();
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, interval);
+        running = true;
+
+    }
+
+    public synchronized void stop() {
+
+        if (!running) {
+            return;
+        }
+
+        task.cancel();
+        running = false;
+
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public long getMSecFromStart() {
+        return System.currentTimeMillis() - started;
+    }
+
+    public double getSecFromStart() {
+        return ((double) getMSecFromStart()) / 1000D;
+    }
+
+}
