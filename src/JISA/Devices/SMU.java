562ab4ddb957e35a9e5c1e7e600de18ed34a5198
William Wood <waw31@cam.ac.uk>
2018-12-22 23:32:09 +0000

    JavaDoc comments.

--- src/JISA/Devices/SMU.java
+++ src/JISA/Devices/SMU.java
@@ -894,12 +894,36 @@ public abstract class SMU extends VISADevice {
         CURRENT
     }
 
+    /**
+     * Enumeration of averaging modes
+     */
     public enum AMode {
+
+        /**
+         * No Averaging.
+         */
         NONE,
+
+        /**
+         * Mean average, taking n data points each time.
+         */
         MEAN_REPEAT,
+
+        /**
+         * Mean average, only taking one new data point each time and using the previous n-1 points.
+         */
         MEAN_MOVING,
+
+        /**
+         * Median average, taking n data points each time.
+         */
         MEDIAN_REPEAT,
-        MEDIAN_MOVING
+
+        /**
+         * Median average, only taking one new data point each time and using the previous n-1 points.
+         */
+        MEDIAN_MOVING;
+
     }
 
 
