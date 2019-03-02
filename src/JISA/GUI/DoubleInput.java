0992f7a226bbc6badd7e294fc9f9ae5917b62e5f
William Wood <waw31@cam.ac.uk>
2019-03-02 15:33:45 +0000

    DoubleInput highlighting

--- src/JISA/GUI/DoubleInput.java
+++ src/JISA/GUI/DoubleInput.java
@@ -5,8 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 
@@ -16,6 +18,7 @@ public class DoubleInput extends HBox {
 
     public TextField    mantissa;
     public TextField    exponent;
+    public HBox         root;
     public DoubleChange onChange = null;
 
     public DoubleInput() {
@@ -23,7 +26,7 @@ public class DoubleInput extends HBox {
         try {
             FXMLLoader loader = new FXMLLoader(DoubleInput.class.getResource("FXML/DoubleInput.fxml"));
             loader.setController(this);
-            Parent root = loader.load();
+            root = loader.load();
             GUI.runNow(() -> getChildren().add(root));
             HBox.setHgrow(root, Priority.ALWAYS);
             mantissa.setTextFormatter(new TextFormatter<Double>(new DoubleStringConverter(), 0.0, DBL_FILTER));
@@ -39,6 +42,30 @@ public class DoubleInput extends HBox {
                 if (onChange != null) {
                     onChange.change(getValue());
                 }
+            });
+
+            mantissa.focusedProperty().addListener((observable, oldValue, newValue) -> {
+
+                if (mantissa.isFocused()) {
+                    root.getStyleClass().add("focused");
+                    root.setEffect(new DropShadow(3, Color.valueOf("#039ED3")));
+                } else {
+                    root.getStyleClass().remove("focused");
+                    root.setEffect(null);
+                }
+
+            });
+
+            exponent.focusedProperty().addListener((observable, oldValue, newValue) -> {
+
+                if (exponent.isFocused()) {
+                    root.getStyleClass().add("focused");
+                    root.setEffect(new DropShadow(3, Color.valueOf("#039ED3")));
+                } else {
+                    root.getStyleClass().remove("focused");
+                    root.setEffect(null);
+                }
+
             });
 
         } catch (Exception ignored) {
