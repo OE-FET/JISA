45ea1acc55b243bdb6d3e8e4cff07691b09e1754
William Wood <waw31@cam.ac.uk>
2020-01-13 18:13:19 +0000

    LinearFit improvements.

--- src/jisa/maths/fits/LinearFit.java
+++ src/jisa/maths/fits/LinearFit.java
@@ -2,16 +2,14 @@ package jisa.maths.fits;
 
 public class LinearFit extends PolyFit {
 
-    public LinearFit(PolyFit fit) {
-        super(fit.getParameters(), fit.getErrors());
-    }
+    public LinearFit(PolyFit fit)     { super(fit.getParameters(), fit.getErrors()); }
 
-    public double getIntercept() {
-        return getParameter(0);
-    }
+    public double getIntercept()      { return getParameter(0); }
 
-    public double getGradient() {
-        return getParameter(1);
-    }
+    public double getGradient()       { return getParameter(1); }
+
+    public double getInterceptError() { return getError(0); }
+
+    public double getGradientError()  { return getError(1); }
 
 }
