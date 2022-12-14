5813947c5683fe08554ea973da91ea84b00eec99
William Wood <waw31@cam.ac.uk>
2022-12-14 19:27:14 +0000

    Updates to SPAs

--- src/jisa/devices/interfaces/SPA.java
+++ src/jisa/devices/interfaces/SPA.java
@@ -11,13 +11,41 @@ public interface SPA extends Instrument, MultiInstrument {
         return "Semiconductor Parameter Analyser";
     }
 
+    /**
+     * Returns a list of all SMU sub-instruments.
+     *
+     * @return List of all SMU units in the SPA.
+     */
     List<SMU> getSMUChannels();
+
+    /**
+     * Returns the SMU sub-instrument of the SPA with the given index.
+     *
+     * @param index Index to get
+     *
+     * @return SMU subunit of given index
+     */
+    default SMU getSMUChannel(int index) {
+        return getSMUChannels().get(index);
+    }
 
     List<VMeter> getVMeterChannels();
 
+    default VMeter getVMeterChannel(int index) {
+        return getVMeterChannels().get(index);
+    }
+
     List<VSource> getVSourceChannels();
 
+    default VSource getVSourceChannel(int index) {
+        return getVSourceChannels().get(index);
+    }
+
     List<Switch> getSwitchChannels();
+
+    default Switch getSwitchChannel(int index) {
+        return getSwitchChannels().get(index);
+    }
 
     @Override
     default List<Class<? extends Instrument>> getSubInstrumentTypes() {
