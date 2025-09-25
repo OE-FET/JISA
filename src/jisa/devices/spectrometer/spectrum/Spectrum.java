2781844a36092fb938e92dc58d1e0a702edb7643
William Wood <waw31@cam.ac.uk>
2025-09-25 20:19:48 +0100

    Prior ProScan and general fixes.

--- src/jisa/devices/spectrometer/spectrum/Spectrum.java
+++ src/jisa/devices/spectrometer/spectrum/Spectrum.java
@@ -109,7 +109,13 @@ public class Spectrum implements Iterable<Spectrum.Point> {
 
     public Spectrum divide(Spectrum other) {
 
-        return new Spectrum(wavelengths, IntStream.range(0, counts.length).mapToDouble(i -> counts[i]/other.counts[i]).toArray(), timestamp);
+        return new Spectrum(wavelengths, IntStream.range(0, counts.length).mapToDouble(i -> {
+            try {
+                return counts[i] / other.counts[i];
+            } catch (Throwable e) {
+                return 0.0;
+            }
+        }).toArray(), timestamp);
 
     }
 
