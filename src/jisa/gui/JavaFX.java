e486e13bb80f449ee134bea58dffac78ae0bd854
William Wood <waw31@cam.ac.uk>
2026-05-18 15:53:41 +0100

    Auto UI scaling for Linux.

--- src/jisa/gui/JavaFX.java
+++ src/jisa/gui/JavaFX.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
@@ -14,6 +15,27 @@ public class JavaFX {
     public static void launch() {
 
         try {
+
+            if (com.sun.jna.Platform.isLinux() && System.getProperty("glass.gtk.uiScale") == null) {
+
+                int dpi;
+                try {
+                    // Short command execution syntax
+                    Process p = Runtime.getRuntime().exec(new String[]{"bash", "-c", "xrdb -query | awk -F':[\t ]*' '/Xft.dpi/ {print $2}'"});
+
+                    // Read all bytes directly and clean the string
+                    String output = new String(p.getInputStream().readAllBytes()).trim();
+
+                    dpi = output.isEmpty() ? 96 : Integer.parseInt(output);
+                } catch (IOException | NumberFormatException e) {
+                    dpi = 96; // Fallback default
+                }
+
+                double factor = (double) dpi / 96.0;
+
+                System.setProperty("glass.gtk.uiScale", String.format("%.02f", factor));
+
+            }
 
             // Start-up the JavaFx GUI thread
             Platform.startup(() -> { });
@@ -28,6 +50,8 @@ public class JavaFX {
             });
 
             latch.tryAcquire(10, TimeUnit.SECONDS);
+
+
 
         } catch (Throwable e) {
             System.err.println("Error initialising JavaFX platform!");
